cf49b09f2c6d9191cc52f4ad437c91f32ae37907
Rajneesh Verma <rjnshvrm23@gmail.com>
2022-11-16 20:02:05 +0530

    added spotify API

--- app/top_100_songs/billboard.py
+++ app/top_100_songs/billboard.py
@@ -3,7 +3,7 @@ import datetime
 import requests
 from bs4 import BeautifulSoup
 
-url="https://www.billboard.com/charts/hot-100"
+url = "https://www.billboard.com/charts/hot-100"
 now = datetime.datetime.now()
 date = input("Please enter a date in yyyy-mm-dd format:\n")
 response = requests.get(f"{url}/{date}")
@@ -12,4 +12,3 @@ soup = BeautifulSoup(response.text, "html.parser")
 top_100 = soup.find_all(name="h3", id="title-of-a-story", class_="a-chart-detail-open")
 song_list = [n.text.strip() for n in top_100]
 print(song_list)
-
--- app/top_100_songs/spotify_api.py
+++ app/top_100_songs/spotify_api.py
@@ -1,17 +1,21 @@
 import os
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
 
 load_dotenv()
 
 # spotify dashboard: https://developer.spotify.com/dashboard/
 # app name: billboard_top100
-scope = "playlist-modify-private"
-redirect_uri = "http://example.com"
 
-sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
-search_str = 'Muse'
-result = sp.search(search_str)
-print(result)
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://localhost:8888/callback",
+        client_id=os.environ.get('SPOTIPY_CLIENT_ID'),
+        client_secret=os.environ.get('SPOTIPY_CLIENT_SECRET'),
+        show_dialog=True,
+        cache_path="token.txt"
+    ))
 
+user_id = sp.current_user()["id"]
+print(user_id)
