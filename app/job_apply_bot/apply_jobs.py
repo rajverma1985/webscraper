c3e99e8f69980e8f0476608c23aa92a3730dea3b
Rajneesh Verma <rjnshvrm23@gmail.com>
2022-11-19 21:10:38 +0530

    added job application submission logic for first job

--- app/job_apply_bot/apply_jobs.py
+++ app/job_apply_bot/apply_jobs.py
@@ -11,6 +11,7 @@ load_dotenv()
 # get the email and password from environment variables
 Email = os.environ.get("EMAIL")
 Password = os.environ.get("PASSWORD")
+Phone = os.environ.get("PHONE")
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get("https://www.linkedin.com/jobs/search/?f_LF=f_AL&geoId=102257491&keywords=python%20developer&location"
            "=London%2C%20England%2C%20United%20Kingdom&redirect=false&position=1&pageNum=0")
@@ -23,6 +24,14 @@ user_name.send_keys(Email)
 password = driver.find_element(By.ID, "password")
 password.send_keys(Password)
 password.send_keys(Keys.ENTER)
+apply_job = driver.find_element(By.CLASS_NAME, "jobs-apply-button--top-card")
+apply_job.click()
+phone_entry = driver.find_element(By.CLASS_NAME, "fb-single-line-text__input")
+if phone_entry.text == "":
+    phone_entry.send_keys(Phone)
+
+submit = driver.find_element(By.CSS_SELECTOR, "footer button")
+submit.click()
 
 # Todo: add functionality to apply to a job with relevant job type/description
 
