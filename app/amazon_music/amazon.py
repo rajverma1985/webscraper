d6aa0edbc5ba299e59d884128932b4c036853e82
Rajneesh Verma <rjnshvrm23@gmail.com>
2022-11-24 06:26:23 +0530

    getting most of the playlist info

--- app/amazon_music/amazon.py
+++ app/amazon_music/amazon.py
@@ -11,14 +11,15 @@ load_dotenv()
 # get the email and password from environment variables
 Amazon_email = os.environ.get("EMAIL")
 Amazon_pass = os.environ.get("PASSWORD")
-url = "https://music.amazon.in/"
+url = "https://music.amazon.in/user-playlists/b85f7027a8bf4ff6badc334f4e93dd8bi8n0?marketplaceId=A3K6Y4MI8GDYMT" \
+      "&musicTerritory=IN&ref=dm_sh_HFphAKldu6jIILo9VAGcs7aPa "
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get(url)
-time.sleep(3)
-# sign in
-driver.find_element(By.XPATH, '//*[@id="music-navbar"]/ul/li[5]').click()
-driver.find_element(By.ID, 'signInButton').click()
-driver.find_element(By.ID, 'ap_email').send_keys(Amazon_email)
-driver.find_element(By.ID, 'ap_password').send_keys(Amazon_pass)
-driver.find_element(By.ID, 'signInSubmit').click()
+driver.maximize_window()
 time.sleep(5)
+playlist = driver.find_elements(By.CSS_SELECTOR, '._3bX1IY2t1o6SakF2rd-yk_ .content .col1')
+time.sleep(5)
+print([p.text for p in playlist])
+
+
+
