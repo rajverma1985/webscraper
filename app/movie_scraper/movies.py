9dd2a12b1caf13757b62935b118690bb97de8509
Rajneesh Verma <rjnshvrm23@gmail.com>
2022-11-29 23:19:54 +0530

    checking bs4

--- app/movie_scraper/movies.py
+++ app/movie_scraper/movies.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 response = requests.get("https://www.empireonline.com/movies/features/best-movies-2")
 soup = BeautifulSoup(response.text, "html.parser")
 movies = soup.find_all(class_='jsx-3821216435 listicle-item')
-
+print(movies)
 
 ## TODO issues with the site structure, need to use selenium for scraping.
 
--- app/top_100_songs/billboard.py
+++ app/top_100_songs/billboard.py
@@ -9,3 +9,4 @@ response = requests.get(f"{url}/{date}")
 soup = BeautifulSoup(response.text, "html.parser")
 songs_100 = soup.find_all("h3", id="title-of-a-story", class_="lrv-u-font-size-16")
 song_list = [song.getText().strip() for song in songs_100]
+print(song_list)
